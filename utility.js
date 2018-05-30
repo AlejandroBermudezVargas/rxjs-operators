007eaac4a19a0087b9d7c0c320d1749664dbcc5b
DESKTOP-MQ1N18H\Alejandro <Alejandro>
2018-05-30 12:49:56 -0600

    Recreate utility Standard input

--- utility.js
+++ utility.js
@@ -1,17 +1,17 @@
-import Rx from 'rxjs';
+import Rx from "rxjs";
 
-export const fromStdIn = ()=>{
-    const stdin = process.stdin;
-    stdin.setRawMode(true);
-    stdin.setEncoding('utf-8');
+export const fromStandardInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf-8");
 
-    const observable = Rx.Observable.fromEvent(stdin,'data');
-    observable.subscribe(key=>{
-        if (key === '\u0003') {
-            process.exit();
-        }
-    })
-    return observable;
+  const observable = Rx.Observable.fromEvent(stdin, "data");
+  
+  observable.subscribe(key => {
+    if (key === "\u0003") process.exit();
+  });
+
+  return observable;
 };
 
-//fromStdIn().subscribe(a=>console.log(a));
+fromStandardInput().subscribe(e => console.log(e));
